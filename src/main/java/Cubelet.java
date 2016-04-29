14b0c3989109fadfc4de1e7f4a986b1053ddd168
Jack Rosenthal <jack@rosenth.al>
2016-04-29 15:25:54 -0600

    add orientation finder

--- src/main/java/Cubelet.java
+++ src/main/java/Cubelet.java
@@ -16,6 +16,7 @@ public class Cubelet {
 
     public Position orientation1;
     public Position orientation2;
+    private Cube cube;
 
     public Cubelet(Color c1, Color c2, Color c3, Position orientation1, Position orientation2) {
         this.color1 = c1;
@@ -35,5 +36,20 @@ public class Cubelet {
                color3 == other.color3 &&
                orientation1.equals(other.orientation1) &&
                (orientation2 != null)?orientation2.equals(other.orientation2):true;
+    }
+
+    public Position orientation(int i, Position pos) {
+        if (i < 1 || i > 3)
+            throw new RuntimeException("Invalid orinetation: " + i);
+        if (i == 1) return orientation1;
+        if (i == 2 && orientation2 != null) return orientation2;
+        if (i == 2) return cube.position(pos.x ^ orientation1.x,
+                                         pos.y ^ orientation1.y,
+                                         pos.z ^ orientation1.z);
+        if (i == 3 && orientation2 == null) return null;
+        if (i == 3) return cube.position(pos.x ^ orientation1.x ^ orientation2.x,
+                                         pos.y ^ orientation1.y ^ orientation2.y,
+                                         pos.z ^ orientation1.z ^ orientation2.z);
+        return null;
     }
 }
